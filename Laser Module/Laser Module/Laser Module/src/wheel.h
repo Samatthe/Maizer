2e86696e805ddbf278c7c974c5338edd895ca779
bmiron <bmiron@umich.edu>
2016-12-19 17:54:36 -0500

    Updated laser module code, for Matt

--- Laser Module/Laser Module/Laser Module/src/wheel.h
+++ Laser Module/Laser Module/Laser Module/src/wheel.h
@@ -1,32 +1,3 @@
-//Check button
-	//If button was previously pressed and is still pressed, do nothing
-	//If button was not pressed and is now, send button click
-	//If button was and now is not, change the prev value
-
-
-//Check left
-	//If button was previously pressed and is still pressed, do nothing
-	//If button was not pressed and is now, send button click
-	//If button was and now is not, change the prev value
-
-
-//Check right
-	//If button was previously pressed and is still pressed, do nothing
-	//If button was not pressed and is now, send button click
-	//If button was and now is not, change the prev value
-
-
-//Check down
-	//If button was previously pressed and is still pressed, do nothing
-	//If button was not pressed and is now, send button click
-	//If button was and now is not, change the prev value
-
-
-//Check up
-	//If button was previously pressed and is still pressed, do nothing
-	//If button was not pressed and is now, send button click
-	//If button was and now is not, change the prev value
-
 #include <inttypes.h>
 
 #define	HIGH	1
@@ -36,14 +7,6 @@
 #define MAX_NEG_SCROLL -100
 
 /********************************************************************/
-
-//Buttons (left, right, up, down)
-/*
- enum Wheel_dir{
-	LEFT, RIGHT, UP, DOWN
-};
-*/
-
 //Buttons (left, right, up, down)
 #define LEFT 0
 #define RIGHT 1
@@ -54,22 +17,23 @@
 
 //Struct to store the important button values and states
 struct Wheel_vals{
-	uint8_t up_val;
+	uint8_t up_val;			//current states
 	uint8_t down_val;
 	uint8_t left_val;
 	uint8_t right_val;
 
-	uint8_t left_state;
+	uint8_t left_state;		//previous states
 	uint8_t right_state;
 	uint8_t up_state;
 	uint8_t down_state;
 
-	int16_t LR_count;
+	int16_t LR_count;		//counts moved
 	int16_t UD_count;
 };
 
 /********************************************************************/
 
+//Declare a struct to be used throughout the .h and .c files, simplifies arguments
 struct Wheel_vals wheel;
 
 /********************************************************************/
@@ -84,20 +48,23 @@ void update_values();
 
 /********************************************************************/
 
-//Updates the values and increments the counter
+//Updates the values in the scroll wheel and increments the counter
 void saveValues(uint8_t *current, uint8_t *previous, int16_t *count, uint8_t up_down);
 
 /********************************************************************/
 
-//If up_down is 1, moves counter up, else moves counter down
+//If up_down is 1, moves counter up, else moves counter down for the wheel
 void increment_count(int *counter, int up_down);
 
 /********************************************************************/
 
+//Returns the state of the the individual buttons
 uint8_t getState(uint8_t button_num);
 
 /********************************************************************/
 
+//Updates the pointers to the scroll values, which are used to determine
+//the number of times to scroll on the computer screen
 void getScroll(int8_t *LR_scroll, int8_t *UD_scroll);
 
 /********************************************************************/
